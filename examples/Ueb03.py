0a141b3cbf5ff27ca118070b4e46f7616469349a
phebon <45487452+phebon@users.noreply.github.com>
2020-06-06 00:35:45 +0200

    Erstes Beispiel
    Dies ist ein erstes Beispiel für das Übungsblatt 03 aus Fehlerrechnung 2. Hier kann die Nutzung von mcfr nachvollzogen werden.

---
+++ examples/Ueb03.py
@@ -0,0 +1,57 @@
+import mcfr
+import matplotlib.pyplot as plt
+from matplotlib.ticker import (MultipleLocator, FormatStrFormatter,
+                               AutoMinorLocator)
+import datetime as d
+
+
+xy_list = [(1450,1180), (2000, 1640), (2500, 2190), (1800, 1760), (1000, 900), (1500, 1200), (2000,1600), (1500, 1400),
+           (1200, 1060), (1800, 1540), (1700, 1460), (1300, 1140), (1850, 1650), (2500, 2400)]
+x_list, y_list = [el[0] for el in xy_list], [el[1] for el in xy_list]
+awerte,bwerte,r = mcfr.lin_Reg(xy_list)
+a,sa = mcfr.rou_val_n_err(awerte[0], awerte[1])[0], mcfr.rou_val_n_err(awerte[0], awerte[1])[1]
+b,sb = mcfr.rou_val_n_err(bwerte[0], bwerte[1])[0], mcfr.rou_val_n_err(bwerte[0], bwerte[1])[1]
+r = mcfr.round_to_n(r,4)
+print(f'Die Gerade mit der Geradengleichung: f(x) = {a}+-{sa} + ({b}+-{sb}) x\n'
+      'Korrelationskoeffizient r = {:.3f}\n'.format(r)+
+      f'Die Koeffizienten lauten also:\n'
+      f'a = {awerte} = {a}+-{sa}\n'
+      f'b = {bwerte} = {b}+-{sb}')
+x_basis = list(range(900,2700,20))
+#y_erw_werte = [awerte[0] + bwerte[0]*el for el in x_basis]
+y_erw_werte = [a + b*el for el in x_basis]
+y_max = [a-sa +(b+sb)*el for el in x_basis]
+y_min = [a+sa +(b-sb)*el for el in x_basis]
+
+fig, ax = plt.subplots(figsize=(25,20))
+ax.scatter(x_list, y_list, marker='x',s=80, label='Angaben zu Ausgaben und Einnahmen')
+ax.plot(x_basis, y_erw_werte,'r',linewidth=3,label=f'Beste Regressionsgerade mit r = {r}')
+ax.plot(x_basis, y_min,'--b',linewidth=2,label='Regressionsgerade minimaler Steigung')
+ax.plot(x_basis, y_max,'-.b',linewidth=2, label='Regressionsgerade maximaler Steigung')
+#ax.plot(x_basis, y_erw_werte2,'g')
+ax.margins(0,.01)
+
+ax.yaxis.set_major_locator(MultipleLocator(100))
+ax.yaxis.set_minor_locator(MultipleLocator(50))
+ax.xaxis.set_major_locator(MultipleLocator(100))
+ax.xaxis.set_minor_locator(MultipleLocator(50))
+
+today = d.datetime.today().strftime('%d.%m.%Y')
+ax.set_title('Streudiagramm von monatlichen Ausgaben in Abhängigkeit von Einnahmen,\n Moritz Siebert, '+ today, fontsize=34, loc = 'right', y=1.015)
+
+ax.set_ylabel('Ausgaben (€)', fontsize=30, labelpad=40)
+ax.set_xlabel('Einnahmen (€)', fontsize=30, labelpad=40)
+
+ax.tick_params(axis='both', which='major', labelsize=20, length=10, direction='in')
+ax.tick_params(axis='both', which='minor', length=5, direction='in')
+
+'''leg = ax.legend(['Angaben zu Ausgaben und Einnahmen', f'Beste Regressionsgerade mit r = {r}','Regressionsgerade minimaler Steigung',
+                 'Regressionsgerade maximaler Steigung'], fontsize=24,
+                title='Legende',title_fontsize=24,
+              frameon=True, loc=(.1, .85))'''
+leg = ax.legend(fontsize=24,
+                title='Legende',title_fontsize=24,
+              frameon=True, loc=(.1, .75))
+leg._legend_box.align = "left"
+
+fig.savefig('Graphisch/Plot.png')
